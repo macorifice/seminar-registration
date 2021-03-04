49655c39aa39636cc237073de27bc56c4d896297
Stefano Di Cecco <s.dicecco@reply.it>
2021-03-04 18:01:04 +0100

    added jest enzyme - render without crashing - render main component correctly

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import Main from './components/Main';
 
-test('renders learn react link', () => {
-  render(<App />);
+configure({ adapter: new Adapter() });
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('main');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe('render without crashing', () => {
+  it('render main app', () => {
+    shallow(<App />);
+  });
+});
+
+describe('render main component correctly', () => {
+  it('render first step', () => {
+    act(() => {
+      ReactDOM.render(<Main />, container);
+    });
+
+    const title = container.querySelector('h1');
+    expect(title.textContent).toBe('Seminar');
+
+    const paragraph = container.querySelector('p');
+    expect(paragraph.textContent).toBe('How many people will be attending ?');
+  });
 });
